c71707c4e5826fe6e9dcb7053046bf2bc98d2604
kuina <kuinanein@gmail.com>
2017-10-22 01:15:25 +0900

    new spring starter project

---
+++ src/main/java/kuina/vue_debug_path/VueDebugPathApplication.java
@@ -0,0 +1,12 @@
+package kuina.vue_debug_path;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VueDebugPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VueDebugPathApplication.class, args);
+	}
+}
---
+++ src/test/java/kuina/vue_debug_path/VueDebugPathApplicationTests.java
@@ -0,0 +1,16 @@
+package kuina.vue_debug_path;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VueDebugPathApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
